c209d2e267c2d4a81e35efd02b9d4b7c44b8c806
jaaseru <julian.aaserud@onio.com>
2024-10-07 10:11:32 +0200

    service edit

--- app/install.sh
+++ app/install.sh
@@ -9,10 +9,10 @@ sudo apt-get update && sudo apt-get upgrade -y
 
 # Clone the git repository
 echo "Cloning git repository..."
-git clone git@github.com:onio-git/gateway.hub.git /home/pi/gateway.hub
+git clone git@github.com:onio-git/gateway.hub.git /home/pi/Desktop/gateway.hub
 
 # Navigate to the project directory
-cd /home/pi/gateway.hub
+cd /home/pi/Desktop/gateway.hub
 
 # Install Python dependencies (if applicable)
 sudo apt install -y python3 python3-pip python3-flask python3-waitress \
@@ -28,6 +28,7 @@ fi
 
 # Set up scripts to run on startup using systemd
 echo "Setting up startup service..."
+cd /home/pi/Desktop/gateway.hub/app
 sudo cp SmarthubManager.service /etc/systemd/system/
 sudo systemctl daemon-reload
 sudo systemctl enable SmarthubManager.service
