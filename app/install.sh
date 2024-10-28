6973691370782efe3887226d5581eeb8c42919f8
jaaseru <julian.aaserud@onio.com>
2024-10-28 12:35:22 +0100

    latest server features

--- app/install.sh
+++ app/install.sh
@@ -65,18 +65,7 @@ sudo systemctl daemon-reload
 sudo systemctl enable SmarthubServer.service
 sudo systemctl restart SmarthubServer.service
 
-# Edit hostname
-echo "Setting up hostname..."
-sudo bash -c 'echo "onio-hub" > /etc/hostname'
-# Edit hosts file:
-sudo bash -c 'echo -e 
-"127.0.0.1	localhost
-::1		localhost ip6-localhost ip6-loopback
-ff02::1		ip6-allnodes
-ff02::2		ip6-allrouters
 
-127.0.1.1		onio-hub"
->> /etc/hosts'
 
 
 
@@ -100,6 +89,19 @@ sudo chmod +x /usr/local/bin/hub_reboot
 # 4. hub_logs
 sudo bash -c 'echo -e "#!/bin/bash\nsudo journalctl -u SmarthubManager.service -f" > /usr/local/bin/hub_logs'
 sudo chmod +x /usr/local/bin/hub_logs
+
+# Edit hostname
+echo "Setting up hostname..."
+sudo bash -c 'echo "onio-hub" > /etc/hostname'
+# Edit hosts file:
+sudo bash -c 'echo -e 
+"127.0.0.1	localhost
+::1		localhost ip6-localhost ip6-loopback
+ff02::1		ip6-allnodes
+ff02::2		ip6-allrouters
+
+127.0.1.1		onio-hub"
+>> /etc/hosts'
 
 # Reboot the system to apply changes
 echo ""
