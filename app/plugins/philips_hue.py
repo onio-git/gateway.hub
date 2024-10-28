c24f4765530a3a0a3ff55c914db34bcc088739d5
nhatnguyen <nhatnguyen@mpire.vn>
2024-10-28 17:20:16 +0700

    test: Add testing
    Add testing

--- app/plugins/philips_hue.py
+++ app/plugins/philips_hue.py
@@ -123,9 +123,9 @@ class philips_hue(PluginInterface):
                 logging.info(f"Connected to {self.mac_address} - {self.device_name}")
 
                 logging.info(f"System Command: {system_command}, meta_data: {meta_data}")
-                attributes = meta_data.get("attributes", {})
-                for data_attribute in attributes:
-                    logging.info(data_attribute)
+                # attributes = meta_data.get("attributes", {})
+                # for data_attribute in attributes:
+                #     logging.info(data_attribute)
                 # if system_command == "turn-on":
                 #     await client.write_gatt_char(LIGHT_CHARACTERISTIC, b'\x01')
                 #     print("Đèn đã được bật" if command == b'\x01' else "Đèn đã được tắt")
