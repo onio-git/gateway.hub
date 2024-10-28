ea0d2b062ddfee716f946ead7adf99ae11e20708
nhatnguyen <nhatnguyen@mpire.vn>
2024-10-28 15:31:19 +0700

    test: Add testing
    Add testing

--- app/plugins/philips_hue.py
+++ app/plugins/philips_hue.py
@@ -156,6 +156,7 @@ class philips_hue(PluginInterface):
                         await client.write_gatt_char(LIGHT_CHARACTERISTIC, command)
                         print("Đèn đã được bật" if command == b'\x01' else "Đèn đã được tắt")
 
+                    logging.info(f"System Command: {system_command}")
                     if system_command == "turn-on":
                         await toggle_light(client, True)
                     elif system_command == "turn-off":
