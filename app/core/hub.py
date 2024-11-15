d40d3ba41f1b1e95e85630a6865f54ab8970d2dd
nhatnguyen <nhatnguyen@mpire.vn>
2024-11-15 17:32:47 +0700

    test: Add testing
    Add testing

--- app/core/hub.py
+++ app/core/hub.py
@@ -35,9 +35,9 @@ class Hub:
         # Comment out the plugins you don't want to load
         # Will later be managed by API
 
-        self.load_plugin("null") # Sensor emulator plugin
+        # self.load_plugin("null") # Sensor emulator plugin
         # self.load_plugin("onio_ble") # ONiO BLE plugin
-        # self.load_plugin("philips_hue")  # Philips hue experimental plugin
+        self.load_plugin("philips_hue")  # Philips hue experimental plugin
 
         # self.load_plugin("xiaomi") # Xiaomi experimental plugin
         # self.load_plugin("sonos") # Sonos plugin
